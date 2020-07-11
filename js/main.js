18cdad1a58e2088c17533d69a8142f640497c2e6
Dim <dcaramello@gmail.com>
2020-07-11 16:08:36 +0200

    add code with loops

--- js/main.js
+++ js/main.js
@@ -8,79 +8,113 @@
 //             / \     ||
 //            /   \    ||
 //                     ||
-// ---------------------------------------
+// -----------------------------------------------------------------------------------------------------------------------
 
 // message bienvenue
 alert("Bienvenue dans le jeu du pendu !");
 
-// variables globales
+// LES VARIABLES
 var scoreTotal = 7;
-const word = [
+const WORDS = [
     "javascript",
     "programming",
     "git",
     "linux",
     "data",
     "server",
-    "client"
+    "client",
+    "php",
+    "github",
+    "terminal",
+    "code",
+    "web",
+    "computer"
 ]
-console.log(word);
-//-----------------------------------------------
+console.log(WORDS);
+//////////////////////////////////////////////////////////////////////////////
 //Choix aléatoire
 function choiceRandomWord (){
-    let randomWord = Math.floor(Math.random() * Math.floor(word.length));
-    return word[randomWord];
+    let randomWord = Math.floor(Math.random() * Math.floor(WORDS.length));
+    return WORDS[randomWord];
 }
-var randomWord = choiceRandomWord(word);
-console.log(randomWord);
-//-------------------------------------------------
-//Affiche : _ _ _ _ _ _
+
+//////////////////////////////////////////////////////////////////////////////
+// Convertir le mot aleatoire en tableau
+function splitWordInArray(randomWord) {
+    let arrayWord = randomWord.split('');
+    return arrayWord;    
+}
+
+//////////////////////////////////////////////////////////////////////////////
+// convertir les éléments du tableau en " _ " pour l'affichage
 function showHiddenLetter (randomWord){
-    let hiddenLetter = "";
+    let hiddenLetter = [];
     for (var i of randomWord){
         hiddenLetter += " _ ";
     }
     return hiddenLetter;
 }
+
+//////////////////////////////////////////////////////////////////////////////
+// Le jeu
+
+//genere un mot random
+var randomWord = choiceRandomWord(WORDS);
+console.log(randomWord);
+
+//split le mot random
+var arrayWord = splitWordInArray(randomWord);
+
+// affiche le mot caché
 var hiddenLetter = showHiddenLetter(randomWord)
 console.log(`Trouves le mot caché : ${hiddenLetter}`);
 alert(`Trouve le mot caché : ${hiddenLetter}`);
-//----------------------------------------------------
-// affiche : rentrer une lettre
-// + rentrer une lettre
-function showPutLetter (){
-    let userChoice = prompt(`Points restants : ${scoreTotal} \n\n${hiddenLetter}\n\nRentrer une lettre !`);
-    return userChoice;
-}
-userChoice = showPutLetter();
-console.log(userChoice);
-//------------------------------------------------------
-//Check de la lettre
-function checkLetter() {
-    let goodChoice = "";
-    var badChoice = "";
-    if (randomWord.includes(userChoice)) {
-        alert(`GG ! ${scoreTotal}`)
-        return goodChoice;     
-    }
+
+// Saisie utilisateur    
+// on boucle tant que le score total n'est pas a 0 et que tous les elements n'ont pas été trouvé.
+while(scoreTotal > 0) {
+    var userChoice = prompt(`Points restants : ${scoreTotal} \n\n${hiddenLetter}\n\nSaisie une lettre !`);
+    console.log(userChoice);
+
+    for (let i = 0; i < arrayWord.length; i ++) {      
+        if (userChoice === arrayWord[i])  {
+            arrayWord[i] = userChoice;
+            alert(`Points restants : ${scoreTotal}\nGG ! On conctinue`)  
+            console.log("win")         
+        }
         else {
-        scoreTotal --, alert(`Try again ! ${scoreTotal}`);
-        return badChoice;    
-    }            
+            scoreTotal --, alert(`Points restants : ${scoreTotal}\nTry again ! `);
+            console.log("loose")         
+        }            
+    }
 }
-var letter = checkLetter();
-console.log(letter);
+
+ 
 
 
-    //si ok 
-        // remplace le _
-        //sinon
-            //perd 1 point
 
-// vérifier si assez de points pour continuer le jeu
-    //si score a 0 perdu et recommence le jeu
-        //sinon ca continu
 
-//vérifier si le mot est complet pour continuer le jeu
-    //si ok stop et WIN
-        //sinon retourne a affiche : _ _
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
