e07219432183247160cc202e3a2d64c952d88439
Dim <dcaramello@gmail.com>
2020-07-12 15:44:47 +0200

    update, correction and indentation of code, delete all concole.log

--- js/main.js
+++ js/main.js
@@ -10,11 +10,12 @@
 //                     ||
 // -----------------------------------------------------------------------------------------------------------------------
 
-// message bienvenue
+// message de bienvenue
 alert("Bienvenue dans le jeu du pendu !");
 
 // LES VARIABLES
-var scoreTotal = 7;
+var scoreTotal = 7; //les points de vie
+var wordInProgress = 0; //pour determiner quand le mot est trouvé
 const WORDS = [
     "javascript",
     "programming",
@@ -29,78 +30,75 @@ const WORDS = [
     "code",
     "web",
     "computer"
-]
-console.log(WORDS);
+];
 
 //////////////////////////////////////////////////////////////////////////////
 // LES FONCTIONS
 
-//Choix aléatoire
+//Choix aléatoire d'un mot dans WORDS
 function choiceRandomWord (){
     let randomWord = Math.floor(Math.random() * Math.floor(WORDS.length));
     return WORDS[randomWord];
 }
 
-//////////////////////////////////////////////////////////////////////////////
-// Convertir le mot aleatoire en tableau
+// Convertir le mot aleatoire en tableau avec la methode split
 function splitRandomWordInArray(randomWord) {
     let arrayWord = randomWord.split('');
     return arrayWord;    
 }
 
-//////////////////////////////////////////////////////////////////////////////
 // convertir les éléments du tableau en " _ " pour l'affichage
 function showHiddenLetter (randomWord){
     let hiddenLetter = [];
-    for (var i of randomWord){
-        hiddenLetter += " _ ";
+    for (let i = 0; i < randomWord.length; i++){
+        hiddenLetter[i] = " _ ";
     }
     return hiddenLetter;
 }
 
+// function pour vérifier q'une seule lettre a été saisie
 function justOneLetter (oneLetter) {
     while(oneLetter.length > 1) {
-        oneLetter = prompt("Une seule lettre svp!");
+        oneLetter = prompt("Attention, il faut rentrer une seule lettre !");
     }
     return oneLetter;
 }
 
 //////////////////////////////////////////////////////////////////////////////
 // Le jeu
+//---------
+// Tant que les points restants n'ont pas atteint 0 la partie continue
+// le joueur saisie une lettre
+// si la lettre est présente dans le tableau arrayWord, elle remplace la valeur correspondante du tableau hiddenLetter 
+// a chaque valeur de hiddenLetter révélée on incrémente un point de wordInProgress. 
+// Si la valeur de wordInProgress est égale au nombre d'élément de arrayWord c'est gagné
 
-//genere un mot random
 var randomWord = choiceRandomWord(WORDS);
-console.log(randomWord);
-
-//split le mot random
 var arrayWord = splitRandomWordInArray(randomWord);
-console.log(arrayWord);
-
-// affiche le mot caché
 var hiddenLetter = showHiddenLetter(randomWord)
-alert(`Trouve le mot caché : ${hiddenLetter}`);
-console.log(`${hiddenLetter}`);
 
+alert(`Trouves le mot caché : ${hiddenLetter}`);
 
 while(scoreTotal > 0) {
-    // Saisie utilisateur
+
     var userChoice = justOneLetter(prompt(`Points restants : ${scoreTotal} \n\n${hiddenLetter}\n\nSaisis une lettre !`));
-    console.log(userChoice);  
-
-
-//check si la lettre est dans le tableau
     if (arrayWord.includes(userChoice))  {
-        console.log("found");
         for (var i = 0; i < arrayWord.length; i ++) {
             if (userChoice.toLowerCase() === arrayWord[i]) {
-            hiddenLetter[i] = userChoice.toLowerCase();
-            console.log(arrayWord);
+                hiddenLetter[i] = userChoice.toLowerCase();
+                wordInProgress ++;    
             }
         }
     }
     else {
-        console.log("not found");        
         scoreTotal --;
+    }
+    if(wordInProgress === arrayWord.length){
+        alert("Félicitation tu as gagné !");
+        break;
+    }
+    else if (scoreTotal === 0) {
+        alert("Perdu !...");
     }
 }
 
